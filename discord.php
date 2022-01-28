6d52be6e4495782230cfe4920ce29e1eeab0b139
Tyler Twining <twijn@twijn.net>
2022-01-27 22:50:45 -0500

    Missed semicolon in discord.php

--- discord.php
+++ discord.php
@@ -1,5 +1,6 @@
 <?php
-require_once("../config.php")
+require_once("../config.php");
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
